0061be77232ab1c65ee46a30c8fd7d61145704b4
Purnendu Talukder <ptsourav21@gmail.com>
2023-08-19 13:35:47 +0600

    Basics of Events
    <article className="book" onMouseOver={()=>{
          console.log(title);
        }}>

--- src/index.js
+++ src/index.js
@@ -54,14 +54,26 @@ function BookList() {
     </>
   );
 }
-const Book = (props) => {
-  const {img, title, author} = props
+const Book = ({img, title, author}) => {
+  //attributes, eventHandler
+  //onClick, onMouseOver
+  const clickHandler=(e)=>{
+    console.log(e);
+    console.log(e.target);
+    alert('Hello World');
+  }
+  const complexExample =(author)=>{
+    console.log(author);
+  }
   return (
-    <article className="book">
+    <article className="book" onMouseOver={()=>{
+      console.log(title);
+    }}>
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
-      
+      <button type="button" onClick={clickHandler}>Reference example</button>
+      <button type="button" onClick={()=>complexExample(author)}>More Complex example</button>
     </article>
   );
 };
